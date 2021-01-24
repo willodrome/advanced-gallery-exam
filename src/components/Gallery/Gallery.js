a40cea00866b1a9bb6127522ff1138ebf3d41251
willodrome <65831821+willodrome@users.noreply.github.com>
2021-01-24 21:05:18 +0200

    Step 2 - Basic implementation

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import axios from 'axios';
-import Image from '../Image';
-import './Gallery.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import Image from "../Image";
+import "./Gallery.scss";
 
 class Gallery extends React.Component {
   static propTypes = {
-    tag: PropTypes.string
+    tag: PropTypes.string,
   };
 
   constructor(props) {
     super(props);
     this.state = {
       images: [],
-      galleryWidth: this.getGalleryWidth()
+      galleryWidth: this.getGalleryWidth(),
     };
   }
 
-  getGalleryWidth(){
+  getGalleryWidth() {
     try {
       return document.body.clientWidth;
     } catch (e) {
@@ -26,21 +26,21 @@ class Gallery extends React.Component {
   }
   getImages(tag) {
     const getImagesUrl = `services/rest/?method=flickr.photos.search&api_key=522c1f9009ca3609bcbaf08545f067ad&tags=${tag}&tag_mode=any&per_page=100&format=json&nojsoncallback=1`;
-    const baseUrl = 'https://api.flickr.com/';
+    const baseUrl = "https://api.flickr.com/";
     axios({
       url: getImagesUrl,
       baseURL: baseUrl,
-      method: 'GET'
+      method: "GET",
     })
-      .then(res => res.data)
-      .then(res => {
+      .then((res) => res.data)
+      .then((res) => {
         if (
           res &&
           res.photos &&
           res.photos.photo &&
           res.photos.photo.length > 0
         ) {
-          this.setState({images: res.photos.photo});
+          this.setState({ images: res.photos.photo });
         }
       });
   }
@@ -48,7 +48,7 @@ class Gallery extends React.Component {
   componentDidMount() {
     this.getImages(this.props.tag);
     this.setState({
-      galleryWidth: document.body.clientWidth
+      galleryWidth: document.body.clientWidth,
     });
   }
 
@@ -59,8 +59,14 @@ class Gallery extends React.Component {
   render() {
     return (
       <div className="gallery-root">
-        {this.state.images.map(dto => {
-          return <Image key={'image-' + dto.id} dto={dto} galleryWidth={this.state.galleryWidth}/>;
+        {this.state.images.map((dto) => {
+          return (
+            <Image
+              key={"image-" + dto.id}
+              dto={dto}
+              galleryWidth={this.state.galleryWidth}
+            />
+          );
         })}
       </div>
     );
--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,29 +1,50 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import FontAwesome from 'react-fontawesome';
-import './Image.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import FontAwesome from "react-fontawesome";
+import Viewer from "react-viewer";
+import "react-viewer/dist/index.css";
+import "./Image.scss";
 
 class Image extends React.Component {
   static propTypes = {
     dto: PropTypes.object,
-    galleryWidth: PropTypes.number
+    galleryWidth: PropTypes.number,
   };
 
   constructor(props) {
     super(props);
     this.calcImageSize = this.calcImageSize.bind(this);
+    this.rotateImage = this.rotateImage.bind(this);
+    this.deleteImage = this.deleteImage.bind(this);
+    this.expandImage = this.expandImage.bind(this);
+    this.angle = [0, 90, 180, 270];
     this.state = {
-      size: 200
+      size: 200,
+      turns: 0,
+      deleted: false,
+      expanded: false,
     };
   }
 
+  rotateImage() {
+    this.setState({ turns: this.state.turns + 1 });
+  }
+
+  deleteImage() {
+    this.setState({ deleted: true });
+  }
+
+  expandImage() {
+    this.setState({ expanded: !this.state.expanded });
+  }
+
   calcImageSize() {
-    const {galleryWidth} = this.props;
+    const { galleryWidth } = this.props;
     const targetSize = 200;
     const imagesPerRow = Math.round(galleryWidth / targetSize);
-    const size = (galleryWidth / imagesPerRow);
+    const size = galleryWidth / imagesPerRow;
     this.setState({
-      size
+      size,
     });
   }
 
@@ -36,19 +57,55 @@ class Image extends React.Component {
   }
 
   render() {
+    const { turns, deleted, expanded } = this.state;
+    const angle = this.angle[turns % 4];
+    const imageClass = `image-root ${deleted && "image-hidden"}`;
     return (
       <div
-        className="image-root"
+        className={imageClass}
         style={{
           backgroundImage: `url(${this.urlFromDto(this.props.dto)})`,
-          width: this.state.size + 'px',
-          height: this.state.size + 'px'
+          width: this.state.size + "px",
+          height: this.state.size + "px",
+          transform: `rotate(${angle}deg)`,
         }}
+      >
+        {/* Modal Image View */}
+        <Viewer
+          visible={expanded}
+          noToolbar={true}
+          images={[{ src: this.urlFromDto(this.props.dto), alt: "" }]}
+          style={{
+            transform: `rotate(${angle}deg) !important`,
+          }}
+          onClose={() => {
+            this.setState({ expanded: false });
+          }}
+        />
+
+        <div
+          style={{
+            transform: `rotate(-${angle}deg)`,
+          }}
         >
-        <div>
-          <FontAwesome className="image-icon" name="sync-alt" title="rotate"/>
-          <FontAwesome className="image-icon" name="trash-alt" title="delete"/>
-          <FontAwesome className="image-icon" name="expand" title="expand"/>
+          <FontAwesome
+            onClick={() => this.rotateImage()}
+            className="image-icon"
+            name="sync-alt"
+            title="rotate"
+          />
+          <FontAwesome
+            onClick={() => this.deleteImage()}
+            className="image-icon"
+            name="trash-alt"
+            title="delete"
+          />
+          <FontAwesome
+            onClick={() => this.expandImage(this.props.dto.id)}
+            className="image-icon"
+            name="expand"
+            title="expand"
+          />
         </div>
       </div>
     );
